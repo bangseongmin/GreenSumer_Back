41ba7cdb146fa122704e4017a4dbccb0ef66a934
bang-star <bang77060986@gmail.com>
2022-11-29 20:07:40 +0900

    #9 - 스프링 부트 프로젝트 시작
    * 스프링 부트 프로젝트 초기화
    * 들어간 초기 기술
      * org.springframework.boot:spring-boot-starter-web
      * org.springframework.boot:spring-boot-starter-actuator
      * org.springframework.boot:spring-boot-devtools
      * org.projectlombok:lombok

---
+++ src/test/java/org/swyg/greensumer/GreenSumerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swyg.greensumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenSumerBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
