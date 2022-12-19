f7b834ffbc921ccf238f2be31290fa8994c006e4
bang-star <bang77060986@gmail.com>
2022-12-19 17:06:38 +0900

    #8- Data REST 설정 게시글, 댓글의 json api 를 자동으로 restful 하게 만들어지도록 설정

--- src/main/java/org/swyg/greensumer/repository/ArticleCommentRepository.java
+++ src/main/java/org/swyg/greensumer/repository/ArticleCommentRepository.java
@@ -1,7 +1,9 @@
 package org.swyg.greensumer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.swyg.greensumer.domain.ArticleComment;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
