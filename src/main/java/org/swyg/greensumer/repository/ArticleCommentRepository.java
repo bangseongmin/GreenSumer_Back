a45a04204ac9ff902a579eac3e45e61f4bfa90a8
bang-star <bangseongmin@naver.com>
2022-12-19 16:31:06 +0900

    Revert "데이터베이스 로직 테스트 정의"

--- src/main/java/org/swyg/greensumer/repository/ArticleCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package org.swyg.greensumer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.swyg.greensumer.domain.ArticleComment;
-
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
-}
