49a5aada2f625f3acb6c2e119579978f78968eee
bang-star <bang77060986@gmail.com>
2022-12-20 18:24:29 +0900

    #30 - View 정렬 기능을 삭제
    테스트 전체 통과 확인

--- src/main/java/org/swyg/greensumer/domain/constant/SearchType.java
+++ src/main/java/org/swyg/greensumer/domain/constant/SearchType.java
@@ -7,8 +7,7 @@ public enum SearchType {
     CONTENT("본문"),
     ID("유저 ID"),
     NICKNAME("닉네임"),
-    HASHTAG("해시태그"),
-    VIEWS("조회수");
+    HASHTAG("해시태그");
 
     @Getter
     private final String description;
