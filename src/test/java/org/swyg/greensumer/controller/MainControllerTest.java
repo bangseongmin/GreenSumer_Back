96ca5454fd4ae3c9bd7a6bea6ff1fb7d39606d86
bang-star <bang77060986@gmail.com>
2022-12-20 15:27:50 +0900

    #23 - 루트 컨트롤러 테스트에서 DisplayName 추가
    가독성을 위해 이름을 추가하고 메소드 이름을 변경함.

--- src/test/java/org/swyg/greensumer/controller/MainControllerTest.java
+++ src/test/java/org/swyg/greensumer/controller/MainControllerTest.java
@@ -1,5 +1,6 @@
 package org.swyg.greensumer.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -8,6 +9,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@DisplayName("View 루트 컨트롤러")
 @WebMvcTest(MainController.class)
 class MainControllerTest {
 
@@ -17,8 +19,9 @@ class MainControllerTest {
         this.mvc = mvc;
     }
 
+    @DisplayName("forward 테스트")
     @Test
-    void givenNothing_whenRequestingRootPage_thenRedirectsToArticlesPage() throws Exception {
+    void givenNothing_whenRequestingRootPage_thenForwardToArticlesPage() throws Exception {
         // Given
 
         // When & Then
