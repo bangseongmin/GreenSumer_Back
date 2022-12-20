629487b9a45f10704c5353f9721d2f31e2af84cb
bang-star <bang77060986@gmail.com>
2022-12-20 14:40:20 +0900

    #22 - 게시판 컨트롤러 구현
    게시판 기본 조회 화면을 위한 컨트롤러 구현하고
    데이터 전달 완료
    게시판 페이지 기본 정렬 속성 부여: 10개 1페이지, 시간 내림차순

--- src/main/java/org/swyg/greensumer/controller/ArticleController.java
+++ src/main/java/org/swyg/greensumer/controller/ArticleController.java
@@ -1,32 +1,42 @@
 package org.swyg.greensumer.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.swyg.greensumer.domain.Article;
-import org.swyg.greensumer.domain.UserAccount;
-import org.swyg.greensumer.domain.constant.RoleType;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.swyg.greensumer.domain.constant.SearchType;
+import org.swyg.greensumer.dto.response.ArticleResponse;
+import org.swyg.greensumer.dto.response.ArticleWithCommentsResponse;
+import org.swyg.greensumer.service.ArticleService;
 
 @Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
-    @GetMapping
-    public List<Article> articles(){
+    private ArticleService articleService;
 
-        return List.of();
+    @GetMapping
+    public Page<ArticleResponse> articles(
+            @RequestParam(required = false) SearchType searchType,
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+    ){
+
+        return articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
     }
 
     @GetMapping("/{article_id}")
-    public Article articles(@PathVariable Long article_id){
+    public ArticleWithCommentsResponse articles(@PathVariable Long article_id){
 
-        log.info("게시글 id: {}", article_id);
-        UserAccount user = UserAccount.of("test", "test", "test@mail.com", "test", RoleType.USER);
-        return Article.of(user,"test", "test", "#test", "test");
+        return ArticleWithCommentsResponse.from(articleService.searchArticle(article_id));
     }
 }
