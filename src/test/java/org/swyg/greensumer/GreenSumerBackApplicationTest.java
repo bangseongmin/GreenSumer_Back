6e8e79406b01410ecbd19e57750ad037da79f084
bang-star <bang77060986@gmail.com>
2023-01-06 23:55:24 +0900

    #36 - 테스트 코드 구현(게시글 목록, 생성 완료)

--- src/test/java/org/swyg/greensumer/GreenSumerBackApplicationTest.java
+++ src/test/java/org/swyg/greensumer/GreenSumerBackApplicationTest.java
@@ -2,9 +2,11 @@ package org.swyg.greensumer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
-class GreenSumerBackApplicationTests {
+class GreenSumerBackApplicationTest {
 
     @Test
     void contextLoads() {
