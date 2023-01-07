3ff39c8ac6092bff9a94edc428946e3707fafb75
bang-star <bang77060986@gmail.com>
2023-01-07 17:42:06 +0900

    #36 - Fixture 메소드 필드를 final 타입으로 전환

--- src/test/java/org/swyg/greensumer/fixture/Fixtures.java
+++ src/test/java/org/swyg/greensumer/fixture/Fixtures.java
@@ -8,25 +8,42 @@ import java.time.Instant;
 import java.util.Set;
 
 public class Fixtures {
+    private static final String token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWF0IjoxNjczMDIwMDYwLCJleHAiOjE2NzU2MTIwNjB9.USuRnODheSfeL65rpXqQOMkVLnOqCtSacrJsLNQXSNg";
+    private static final Integer id = 1;
+    private static final int price = 10000;
+    private static final int stock = 3;
+    private static final String title = "title";
+    private static final String content = "content";
+    private static final String hashtag = "hashtag";
+    private static final String image = "image";
+    private static final String username = "username";
+    private static final String password = "password";
+    private static final String email = "email";
+    private static final String nickname = "nickname";
+    private static final String name = "name";
+    private static final String address = "address";
+    private static final String description = "description";
+    private static final String lat = "lat";
+    private static final String lng = "lng";
+    private static final String hours = "hours";
+    private static final String logo = "logo";
+    private static final UserRole userRole = UserRole.USER;
+    private static final UserRole sellerRole = UserRole.SELLER;
 
     public static ReviewPost getReviewPost() {
-        final ReviewPost reviewPost = new ReviewPost(1, "title", "content", "hashtag", "imagePath", getProduct(), getUser(), getNow(), null, null);
-        return reviewPost;
+        return new ReviewPost(id, title, content, hashtag, image, getProduct(), getUser(), getNow(), null, null);
     }
 
     public static Product getProduct() {
-        final Product product = new Product(1, "name", 2000, 3, "description", "image", getNow(), null, null);
-        return product;
+        return new Product(id, name, price, stock, description, image, getNow(), null, null);
     }
 
     public static User getUser() {
-        final User user = new User(1, "username", "password", "email", "nickname", "address", "lat", "lng", UserRole.USER, getNow(), null, null);
-        return user;
+        return new User(id, username, password, email, nickname, address, lat, lng, userRole, getNow(), null, null);
     }
 
     public static Store getStore() {
-        final Store store = new Store(1, getUser(), "name", "description", "address", "hours", "lat", "lng", "logo", getNow(), null, null);
-        return store;
+        return new Store(id, getUser(), name, description, address, hours, lat, lng, logo, getNow(), null, null);
     }
 
     private static Timestamp getNow() {
@@ -34,13 +51,15 @@ public class Fixtures {
     }
 
     public static ReviewComment getReviewComment() {
-        final ReviewComment reviewComment = new ReviewComment(1, "content", getUser(), getReviewPost(), getNow(), null, null);
-        return reviewComment;
+        return new ReviewComment(id, content, getUser(), getReviewPost(), getNow(), null, null);
     }
 
     public static ReviewPostWithComment getReviewPostWithComment() {
-        final ReviewPostWithComment reviewPostWithComment = new ReviewPostWithComment(1, "title", "content", "hasttag", "imagepath", getProduct(), getUser(), Set.of(getReviewComment()), getNow(), null, null);
-        return reviewPostWithComment;
+        return new ReviewPostWithComment(id, title, content, hashtag, image, getProduct(), getUser(), Set.of(getReviewComment()), getNow(), null, null);
+    }
+
+    public static String getToken() {
+        return token;
     }
 
 }
