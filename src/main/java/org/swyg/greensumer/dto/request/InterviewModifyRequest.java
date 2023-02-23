7f0701355f27442a1ed8c4fc887d9851ab471c0a
bang-star <bangseongmin@naver.com>
2023-02-23 23:29:56 +0900

    #65 - [Controller] 인터뷰 컨트럴러 테스트

--- src/main/java/org/swyg/greensumer/dto/request/InterviewModifyRequest.java
+++ src/main/java/org/swyg/greensumer/dto/request/InterviewModifyRequest.java
@@ -2,14 +2,15 @@ package org.swyg.greensumer.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.swyg.greensumer.domain.constant.UserRole;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class InterviewModifyRequest {
-    private int interviewId;
     private String writer;
     private String store;
     private String opinion;
-    private UserRole target;
+    private String target;
 }
