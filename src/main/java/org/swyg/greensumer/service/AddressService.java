7ff92f577f0c54ad8be8366094c351b118435e70
bang-star <bangseongmin@naver.com>
2023-03-08 22:54:14 +0900

    #115 - 주소 정보 캐싱
    주소 정보를 캐싱하여 사용하도록 변경함.

--- src/main/java/org/swyg/greensumer/service/AddressService.java
+++ src/main/java/org/swyg/greensumer/service/AddressService.java
@@ -4,30 +4,67 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.swyg.greensumer.domain.AddressEntity;
+import org.swyg.greensumer.dto.Address;
 import org.swyg.greensumer.exception.ErrorCode;
 import org.swyg.greensumer.exception.GreenSumerBackApplicationException;
+import org.swyg.greensumer.repository.store.AddressCacheRepository;
 import org.swyg.greensumer.repository.store.AddressEntityRepository;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class AddressService {
 
     private final AddressEntityRepository addressEntityRepository;
+    private final AddressCacheRepository addressCacheRepository;
 
     public AddressEntity findAddressEntity(String address, String roadname, Double latitude, Double longitude) {
-        return addressEntityRepository.findByLatitudeAndLongitude(latitude, longitude).orElseGet(
-                () -> addressEntityRepository.save(AddressEntity.builder().address(address).roadname(roadname).latitude(latitude).longitude(longitude).build())
-        );
+        List<Address> addressList = addressCacheRepository.findAll();
+
+        Address target = addressList.stream().filter(e -> e.getLat().equals(latitude) && e.getLng().equals(longitude)).findFirst().get();
+
+        if(!Objects.isNull(target)) {
+            return toEntity(target);
+        }else {
+            AddressEntity entity = AddressEntity.builder().address(address).roadname(roadname).latitude(latitude).longitude(longitude).build();
+
+            AddressEntity addressEntity = addressEntityRepository.findByLatitudeAndLongitude(entity.getLatitude(), entity.getLongitude()).orElseGet(
+                    () -> addressEntityRepository.save(entity));
+
+            addressCacheRepository.saveAddress(Address.fromEntity(addressEntity));
+            return addressEntity;
+        }
     }
 
     public AddressEntity searchAddress(Long id) {
-        return addressEntityRepository.findById(id).orElseThrow(() -> new GreenSumerBackApplicationException(ErrorCode.ADDRESS_NOT_FOUND, String.format("%s not founded", id)));
+        Address address = addressCacheRepository.findById(id).orElseGet(() ->
+                Address.fromEntity(addressEntityRepository.findById(id).orElseThrow(() ->
+                        new GreenSumerBackApplicationException(ErrorCode.ADDRESS_NOT_FOUND, String.format("%s not founded", id)))));
+
+        return toEntity(address);
+
     }
 
     @Transactional
     public AddressEntity updateAddress(Long id, String address, String roadname, double latitude, double longitude) {
         AddressEntity addressEntity = searchAddress(id);
 
-        return addressEntity.updateAddress(address, roadname, latitude, longitude);
+        addressCacheRepository.saveAddress(Address.fromEntity(addressEntity.updateAddress(address, roadname, latitude, longitude)));
+
+        return addressEntity;
     }
+
+    private AddressEntity toEntity(Address address) {
+        return AddressEntity.builder()
+                .id(address.getId())
+                .address(address.getAddress())
+                .roadname(address.getRoadName())
+                .latitude(address.getLat())
+                .longitude(address.getLng())
+                .build();
+    }
+
 }
