85b953a2da6045cb7e2cb27ed6034f95ec9461f8
bang-star <bangseongmin@naver.com>
2023-03-18 22:00:32 +0900

    #138-Retry 적용
    외부 API 호출로 인한 문제가 발생하였을 경우, 재시도처리하여 서비스에 영향을 주지 않도록 하기 위함.

---
+++ src/main/java/org/swyg/greensumer/config/RetryConfig.java
@@ -0,0 +1,9 @@
+package org.swyg.greensumer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@EnableRetry
+@Configuration
+public class RetryConfig {
+}
